e8ee2a6ae361ed864114519da90b1ade2ef202a6
UYasher <shriyash.upadhyaya@gmail.com>
2020-01-25 13:41:44 -0500

    Initialized Problem Set 2

---
+++ PS2/hw2_skeleton.py
@@ -0,0 +1,136 @@
+#############################################################
+## ASSIGNMENT 2 CODE SKELETON
+## RELEASED: 1/29/2019
+## DUE: 2/5/2019
+## DESCRIPTION: In this assignment, you will explore the
+## text classification problem of identifying complex words.
+## We have provided the following skeleton for your code,
+## with several helper functions, and all the required
+## functions you need to write.
+#############################################################
+
+from collections import defaultdict
+import gzip
+
+#### 1. Evaluation Metrics ####
+
+## Input: y_pred, a list of length n with the predicted labels,
+## y_true, a list of length n with the true labels
+
+## Calculates the precision of the predicted labels
+def get_precision(y_pred, y_true):
+    ## YOUR CODE HERE...
+
+    return precision
+    
+## Calculates the recall of the predicted labels
+def get_recall(y_pred, y_true):
+    ## YOUR CODE HERE...
+
+    return recall
+
+## Calculates the f-score of the predicted labels
+def get_fscore(y_pred, y_true):
+    ## YOUR CODE HERE...
+
+    return fscore
+
+#### 2. Complex Word Identification ####
+
+## Loads in the words and labels of one of the datasets
+def load_file(data_file):
+    words = []
+    labels = []   
+    with open(data_file, 'rt', encoding="utf8") as f:
+        i = 0
+        for line in f:
+            if i > 0:
+                line_split = line[:-1].split("\t")
+                words.append(line_split[0].lower())
+                labels.append(int(line_split[1]))
+            i += 1
+    return words, labels
+
+### 2.1: A very simple baseline
+
+## Makes feature matrix for all complex
+def all_complex_feature(words):
+
+## Labels every word complex
+def all_complex(data_file):
+    ## YOUR CODE HERE...
+    performance = [precision, recall, fscore]
+    return performance
+
+
+### 2.2: Word length thresholding
+
+## Makes feature matrix for word_length_threshold
+def length_threshold_feature(words, threshold):
+
+## Finds the best length threshold by f-score, and uses this threshold to
+## classify the training and development set
+def word_length_threshold(training_file, development_file):
+    ## YOUR CODE HERE
+    training_performance = [tprecision, trecall, tfscore]
+    development_performance = [dprecision, drecall, dfscore]
+    return training_performance, development_performance
+
+### 2.3: Word frequency thresholding
+
+## Loads Google NGram counts
+def load_ngram_counts(ngram_counts_file): 
+   counts = defaultdict(int) 
+   with gzip.open(ngram_counts_file, 'rt') as f: 
+       for line in f:
+           token, count = line.strip().split('\t') 
+           if token[0].islower(): 
+               counts[token] = int(count) 
+   return counts
+
+# Finds the best frequency threshold by f-score, and uses this threshold to
+## classify the training and development set
+
+## Make feature matrix for word_frequency_threshold
+def frequency_threshold_feature(words, threshold, counts):
+
+def word_frequency_threshold(training_file, development_file, counts):
+    ## YOUR CODE HERE
+    training_performance = [tprecision, trecall, tfscore]
+    development_performance = [dprecision, drecall, dfscore]
+    return training_performance, development_performance
+
+### 2.4: Naive Bayes
+        
+## Trains a Naive Bayes classifier using length and frequency features
+def naive_bayes(training_file, development_file, counts):
+    ## YOUR CODE HERE
+    training_performance = (tprecision, trecall, tfscore)
+    development_performance = (dprecision, drecall, dfscore)
+    return development_performance
+
+### 2.5: Logistic Regression
+
+## Trains a Naive Bayes classifier using length and frequency features
+def logistic_regression(training_file, development_file, counts):
+    ## YOUR CODE HERE    
+    training_performance = (tprecision, trecall, tfscore)
+    development_performance = (dprecision, drecall, dfscore)
+    return development_performance
+
+### 2.7: Build your own classifier
+
+## Trains a classifier of your choosing, predicts labels for the test dataset
+## and writes the predicted labels to the text file 'test_labels.txt',
+## with ONE LABEL PER LINE
+
+
+if __name__ == "__main__":
+    training_file = "data/complex_words_training.txt"
+    development_file = "data/complex_words_development.txt"
+    test_file = "data/complex_words_test_unlabeled.txt"
+
+    train_data = load_file(training_file)
+    
+    ngram_counts_file = "ngram_counts.txt.gz"
+    counts = load_ngram_counts(ngram_counts_file)
