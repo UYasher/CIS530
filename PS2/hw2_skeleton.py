21f75869066f7704800be9c825eb3a85d799f958
Joongwon Kim <danieljkim0118@gmail.com>
2020-01-26 18:42:40 -0500

    precision-recall curve

--- PS2/hw2_skeleton.py
+++ PS2/hw2_skeleton.py
@@ -16,6 +16,7 @@ from scipy.stats import zscore
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
 
 #### 1. Evaluation Metrics ####
@@ -124,12 +125,12 @@ def length_threshold_feature(words, threshold):
 
 ## Finds the best length threshold by f-score, and uses this threshold to
 ## classify the training and development set
-def word_length_threshold(training_file, development_file):
+def word_length_threshold(training_file, development_file, threshold):  # Remove the threshold parameter after plotting
     twords, y_true_training = load_file(training_file)
     dwords, y_true_development = load_file(development_file)
 
-    y_pred_training = length_threshold_feature(twords, 0)
-    y_pred_development = length_threshold_feature(dwords, 0)
+    y_pred_training = length_threshold_feature(twords, threshold)
+    y_pred_development = length_threshold_feature(dwords, threshold)
 
     tprecision = get_precision(y_pred_training, y_true_training)
     trecall = get_recall(y_pred_training, y_true_training)
@@ -143,6 +144,34 @@ def word_length_threshold(training_file, development_file):
     training_performance = [tprecision, trecall, tfscore]
     development_performance = [dprecision, drecall, dfscore]
     return training_performance, development_performance
+
+
+# Plots Precision-Recall Curve over different thresholds for either word length or frequency
+def plot_curve(training_file, development_file, thresholds, use_length):
+    train_recall = []
+    train_prec = []
+    dev_recall = []
+    dev_prec = []
+    for th in thresholds:
+        if use_length:
+            train, dev = word_length_threshold(training_file, development_file, th)
+        else:
+            train, dev = word_frequency_threshold(training_file, development_file, th)
+        train_recall.append(train[1])
+        train_prec.append(train[0])
+        dev_recall.append(dev[1])
+        dev_prec.append(dev[0])
+    fig, axes = plt.subplots(1, 2)
+    feat = 'length' if use_length else 'frequency'
+    fig.suptitle('Precision-Recall Curve for word %s from thresholds %d to %d' % (feat, thresholds[0], thresholds[-1]))
+    axes[0].plot(train_recall, train_prec)
+    axes[0].title('Training data')
+    axes[1].plot(dev_recall, dev_prec)
+    axes[1].title('Development data')
+    for ii in range(2):
+        axes[ii].xlabel('Recall')
+        axes[ii].ylabel('Precision')
+    plt.show()
 
 ## Make feature matrix consisting of word lengths
 def length_feature(words):
@@ -188,7 +217,9 @@ def word_frequency_threshold(training_file, development_file, counts):
     dwords, dlabels = load_file(development_file)
     toutputs = frequency_threshold_feature(twords, threshold, counts)
     doutputs = frequency_threshold_feature(dwords, threshold, counts)
+    print("=====Training=====")
     tprecision, trecall, tfscore = test_predictions(toutputs, tlabels)
+    print("=====Development=====")
     dprecision, drecall, dfscore = test_predictions(doutputs, dlabels)
     training_performance = [tprecision, trecall, tfscore]
     development_performance = [dprecision, drecall, dfscore]
@@ -225,7 +256,9 @@ def naive_bayes(training_file, development_file, counts):
     y_pred_training = clf.predict(X_train)
     y_pred_development = clf.predict(X_dev)
 
+    print("=====Training=====")
     tprecision, trecall, tfscore = test_predictions(y_pred_training, y_true_training)
+    print("=====Development=====")
     dprecision, drecall, dfscore = test_predictions(y_pred_development, y_true_development)
 
     training_performance = (tprecision, trecall, tfscore)
@@ -248,7 +281,9 @@ def logistic_regression(training_file, development_file, counts):
     y_pred_training = clf.predict(X_train)
     y_pred_development = clf.predict(X_dev)
 
+    print("=====Training=====")
     tprecision, trecall, tfscore = test_predictions(y_pred_training, y_true_training)
+    print("=====Development=====")
     dprecision, drecall, dfscore = test_predictions(y_pred_development, y_true_development)
 
     training_performance = (tprecision, trecall, tfscore)
