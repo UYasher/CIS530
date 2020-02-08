e627a3684496aae6f8af17052f0c9bf635b40937
UYasher <shriyash.upadhyaya@gmail.com>
2020-02-08 15:14:19 -0500

    Implemented rank plays

--- PS4/main.py
+++ PS4/main.py
@@ -218,7 +218,11 @@ def rank_plays(target_play_index, term_document_matrix, similarity_fn):
     '''
 
     # YOUR CODE HERE
-    return []
+
+    target_play_vector = term_document_matrix[:, target_play_index]
+    target_similarity = lambda x: similarity_fn(target_play_vector, x)
+
+    return np.apply_along_axis(target_similarity, 0, term_document_matrix)
 
 
 def rank_words(target_word_index, matrix, similarity_fn):
