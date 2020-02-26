f8e1c97b64a0cdc9850320e0746e91856d533430
UYasher <shriyash.upadhyaya@gmail.com>
2020-02-26 12:17:37 -0500

    Implemented CharRNNClassify -- have not provided defaults

--- PS6/models.py
+++ PS6/models.py
@@ -8,13 +8,23 @@ Please add default values for all the parameters of __init__.
 '''
 class CharRNNClassify(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
-        pass
+        super(CharRNNClassify, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, input, hidden=None):
-        pass
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
 
     def init_hidden(self):
-        pass
+        return torch.zeroes(1, self.hidden_size)
 
 dtype = torch.float
 # device = torch.device("cpu")
