300d0b8dd79fba4c39a19b5e248bf5aad9352844
danieljkim <danieljkim0118@gmail.com>
2020-03-01 20:00:57 -0500

    PS7 added

--- PS6/models.py
+++ PS6/models.py
@@ -29,6 +29,31 @@ class CharRNNClassify(nn.Module):
     def init_hidden(self):
         return torch.randn(1, self.hidden_size)
 
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, n_layers=1):
+        super(RNN, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+
+        self.encoder = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers)
+        self.decoder = nn.Linear(hidden_size, output_size)
+
+    def forward(self, input, hidden):
+        input = self.encoder(input.view(1, -1))
+        output, hidden = self.gru(input.view(1, 1, -1), hidden)
+        output = self.decoder(output.view(1, -1))
+        return output, hidden
+
+    def init_hidden(self):
+        return torch.zeros(self.n_layers, 1, self.hidden_size)
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 # dtype = torch.float
 # device = torch.device("cuda:0")
 # x = torch.randn(600, 50, device=device, dtype=dtype)
