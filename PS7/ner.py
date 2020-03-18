b6dbc00678612c16ca056f7e942aed4e058d6b17
Joongwon Kim <danieljkim0118@gmail.com>
2020-03-18 18:19:30 -0500

    part-of-speech features

--- PS7/ner.py
+++ PS7/ner.py
@@ -1,4 +1,6 @@
+import nltk
 from nltk.corpus import conll2002
+from nltk import pos_tag
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import precision_recall_fscore_support
@@ -6,33 +8,50 @@ from sklearn.metrics import precision_recall_fscore_support
 # Assignment 7: NER
 # This is just to help you get going. Feel free to
 # add to or modify any part of it.
+nltk.download('averaged_perceptron_tagger')
 
 
+# Assigns features in tuples of 'feature name' and actual feature
 def getfeats(word, o):
     """ This takes the word in question and
     the offset with respect to the instance
     word """
     o = str(o)
     features = [
-        (o + 'word', word)
+        (o + 'word', word),
         # TODO: add more features here.
+        # (o + 'pos', pos)
     ]
     return features
-    
 
+
+# Adds part-of-speech features
+def getfeats_pos(pos, o):
+    o = str(o)
+    features = [
+        (o + 'pos', pos)
+    ]
+    return features
+
+
+# Retrieves features for the ith word (starting at 0) in the sentence 'sent'
 def word2features(sent, i):
     """ The function generates all features
     for the word at position i in the
     sentence."""
     features = []
     # the window around the token
-    for o in [-1,0,1]:
-        if i+o >= 0 and i+o < len(sent):
+    window_size = 3
+    for o in range(-1 * window_size, window_size + 1):
+        if 0 <= i + o < len(sent):
             word = sent[i+o][0]
+            pos = sent[i+o][1]
             featlist = getfeats(word, o)
+            featlist_pos = getfeats_pos(pos, o)
             features.extend(featlist)
-    
+            features.extend(featlist_pos)
     return dict(features)
+
 
 if __name__ == "__main__":
     # Load the training data
@@ -45,12 +64,15 @@ if __name__ == "__main__":
 
     for sent in train_sents:
         for i in range(len(sent)):
-            feats = word2features(sent,i)
+            feats = word2features(sent, i)
             train_feats.append(feats)
             train_labels.append(sent[i][-1])
 
     vectorizer = DictVectorizer()
+    print(len(train_feats))
+    print(train_feats[:4])
     X_train = vectorizer.fit_transform(train_feats)
+    print(X_train.shape)
 
     # TODO: play with other models
     model = Perceptron(verbose=1)
@@ -73,7 +95,7 @@ if __name__ == "__main__":
     print("Writing to results.txt")
     # format is: word gold pred
     with open("results.txt", "w") as out:
-        for sent in dev_sents: 
+        for sent in dev_sents:
             for i in range(len(sent)):
                 word = sent[i][0]
                 gold = sent[i][-1]
